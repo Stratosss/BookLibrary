1b9b39e5df92d8c87cb40293dab9485d91b486f1
Stratos <157527268+Stratosss@users.noreply.github.com>
2025-06-02 18:04:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import {Strategy} from "passport-local";
 import env from "dotenv";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const saltRounds = 10;
 env.config();
 
