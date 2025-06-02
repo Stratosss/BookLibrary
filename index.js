a516060ea08ddd35bfbdf83f651247a0a9b8c048
Stratos <157527268+Stratosss@users.noreply.github.com>
2025-06-02 16:05:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ const saltRounds = 10;
 env.config();
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 db.connect();
 
